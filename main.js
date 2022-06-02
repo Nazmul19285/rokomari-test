bcf3e24a98b39476c6b052e1b68afdabd06ac82b
Nazmul19285 <nazmulhossain19285@gmail.com>
2022-06-02 16:46:51 +0600

    video section completed

---
+++ main.js
@@ -0,0 +1,12 @@
+const video = document.querySelector('.video');
+const backgroundVideo = video.querySelector('#background-video')
+const playButton = video.querySelector('.play-button');
+
+playButton.addEventListener('click', (e) => {
+    if(backgroundVideo.paused){
+        backgroundVideo.play();
+    }
+    else{
+        backgroundVideo.pause();
+    }   
+})
